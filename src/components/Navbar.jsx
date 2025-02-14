8e0d6601b37603ce8a5edbad89137d6e831a45ff
karan <kj382542@gmail.com>
2025-02-14 16:14:36 +0530

    github url add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,14 +26,15 @@ const Navbar = () => {
           </a>
         </div>
 
-        <button
-          onClick={() => (window.location.href = "tel:+918595998512")}
+        <a
+          href="https://github.com/karan8509?tab=repositories"
           className="bg-gradient-to-r from-green-400 to-blue-500 text-white 
              transform transition-transform duration-300 hover:scale-105 
              px-6 py-2 rounded-full shadow-lg hover:shadow-xl"
         >
+          {" "}
           Contact me
-        </button>
+        </a>
       </div>
     </nav>
   );
